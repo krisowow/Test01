164647ed0c3ff7161a0703a0020a46ffa9cf9136
krisowow <krisirealm@hotmail.com>
2017-01-21 12:30:05 +0200

    adding files

---
+++ Greeting/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Greeting
+{
+    class SquareOfStars
+    {
+        static void Main(string[] args)
+        {
+            var budget = double.Parse(Console.ReadLine());
+            string category = Console.ReadLine();
+            var people = int.Parse(Console.ReadLine());
+            var vip = 499.99;
+            var normal = 249.99;
+
+
+            
+
+
+
+
+
+
+
+
+        }
+
+      }
+
+  }
+
+
+
+
